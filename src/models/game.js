a557d616c7b93a0f03520bff5b8ee15c7df1e777
Khasim Sheik <khasimsheik@Khasims-MacBook-Pro.local>
2026-04-03 19:12:36 +0530

    [Refactor] | Removes unnecessary console.logs | Khasim / Vikas

--- src/models/game.js
+++ src/models/game.js
@@ -181,7 +181,6 @@ export default class Game {
 
   #isValidDestination({ x, y }) {
     const destinations = this.#board.destinations;
-    console.log({ destinations });
     return destinations.some(
       ({ destination }) => destination.x === x && destination.y === y,
     );
