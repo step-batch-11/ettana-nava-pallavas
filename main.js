6bf3e7d43c55e3ae55608f80039c715579992954
Jeniffer Jojo <jenifferjojo@jeni-Mac.local>
2026-04-04 12:09:46 +0530

    [#20] | adds all design cards to player | Jeniffer

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const main = () => {
   const player1 = new Player(1, "Ajoy");
   player1.setup(1, { x: 1, y: 1 });
 
-  // player1.addAllDesignCardDev(...designCards);
+  player1.addAllDesignCardDev(...designCards);
   player1.addActionCard({
     id: 6,
     "type": "tax",
