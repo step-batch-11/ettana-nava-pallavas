c3f68805623ccf0a695bd85aed5eb384702d8aa7
Jeniffer Jojo <jenifferjojo@jeni-Mac.local>
2026-04-09 12:21:52 +0530

    [#44] | renames action_card_service_test -> action_card_test | Jeniffer/Ajoy

--- test/models/action_card_test/victory_point_card_test.js
+++ test/models/action_card_test/victory_point_card_test.js
@@ -11,31 +11,16 @@ import {
   getAllActionCard,
   getAllDesignCard,
   isPresent,
-  mockTiles,
-  mockYarns,
 } from "../../../src/utils/mock_data.js";
 import VictoryPoint from "../../../src/models/action_cards/victoryPoint.js";
-import Tax from "../../../src/models/action_cards/tax.js";
 
 describe("Action cards", () => {
   let game, players, bank;
-  const yarns = mockYarns();
-  const tiles = mockTiles();
 
   beforeEach(() => {
     players = [new Player(1, "Ajoy"), new Player(2, "Dinesh")];
-    bank = new Bank(
-      getAllDesignCard(),
-      getAllActionCard(),
-    );
-    game = new Game(
-      players,
-      bank,
-      new Board(tiles, yarns),
-      diceValue,
-      Math.random,
-      0,
-    );
+    bank = new Bank(getAllDesignCard(), getAllActionCard());
+    game = new Game(players, bank, new Board([], []), diceValue, Math.random);
   });
 
   describe("Play Victory Point Action Card", () => {
@@ -45,32 +30,14 @@ describe("Action cards", () => {
       players[0].addActionCard(ac);
       players[1].addActionCard(ac);
 
-      VictoryPoint.play(cardId, game);
+      const result = VictoryPoint.play(cardId, game);
 
       const playerVPCardsAfter = players[0].getVp();
 
       assertEquals(playerVPCardsAfter, 1);
       assertEquals(isPresent(players[0].getAc(), ac), false);
       assertEquals(isPresent(players[1].getAc(), ac), true);
+      assertEquals(result, { message: "Victory point added to the player" });
     });
   });
-
-  describe("Play Tax Action Card", () => {
-    it("should collect tax from other players", () => {
-      const cardId = acMap.tax;
-      const ac = getActionCard(cardId);
-
-      players[0].addActionCard(ac);
-      players[1].creditTokens(1);
-
-      Tax.play(cardId, game);
-
-      const actionCardOnHand = players[0].getAc();
-
-      assertEquals(actionCardOnHand, []);
-      assertEquals(players[1].getTokens(), 0);
-      assertEquals(isPresent(actionCardOnHand, ac), false);
-    });
-  });
-
 });
